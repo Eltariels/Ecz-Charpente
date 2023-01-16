46d049c18652f65bd8a1f91a46458cb65ea2ed69
Eltariels <theo.borella.hc@gmail.com>
2023-01-16 11:14:21 +0100

    Change skin scrollbar main

--- functions.php
+++ functions.php
@@ -1,5 +1,7 @@
 <?php
 
-wp_enqueue_style('app', get_template_directory_uri().'/css/app.css', array(), 1, 'all');
+if(!is_admin()) {
+    wp_enqueue_style('app', get_template_directory_uri().'/css/app.css', array(), 1, 'all');
+}
 
 ?>
