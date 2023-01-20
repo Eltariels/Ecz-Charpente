24678d5dda7737d3bbb5ac8f0486d8d6c91188ba
Eltariels <theo.borella.hc@gmail.com>
2023-01-20 09:45:36 +0100

    edit maj header

--- header.php
+++ header.php
@@ -33,12 +33,96 @@
     <meta name="copyright" content="All rights reserved to Mr. BORELLA Théo">
     <!-- ---------- COPYRIGHTS ------------------- -->
 
-    <link rel=" icon" type="img/svg" href="img/svg/">
+    <!-- <link rel=" icon" type="img/svg" href="img/svg/"> -->
     <!-- ---------- ICON HEAD ------------------- -->
     <?php wp_head(); ?>
 </head>
 
-<body>
-    <header>
-        <h1><?php echo (get_the_title()); ?></h1>
-    </header>
+<?php
+
+//NavBar
+$navbar = get_field('navbar', 'options');
+
+//MainTitle
+$maintitlehome = get_field('main_title_home');
+//SecondaryTitle
+$secondtitlehome = get_field('second_title_home');
+
+//Background Image Home
+$bgimghome = get_field('bgimghome', 'option');
+
+//Logo Nav Home
+$logonav = get_field('logonav', 'option');
+
+//Icon Home
+$iconhome = get_field('iconhome', 'option');
+
+//debug($navbar);
+?>
+
+<body <?php body_class(); ?>>
+
+    <?php if (is_front_page()) : ?>
+
+        <header class="PrimaryHeader" style="background-image: url(<?php if (!empty($bgimghome)) : ?><?php echo ($bgimghome['url']); ?><?php endif; ?>) ">
+            <div class="HomeFilter"></div>
+            <div class="HeaderNav">
+                <div class="NavContainer Img">
+                    <?php if (!empty($logonav)) : ?>
+                        <a href="<?php echo (get_home_url()); ?>"><img src="<?php echo ($logonav['url']); ?>" alt="<?php echo ($logonav['alt']); ?>" /></a>
+                    <?php endif; ?>
+                </div>
+                <?php wp_nav_menu(array(
+                    'menu' => 'main_menu',
+                    'container_class' => 'NavContainer Nav',
+                    'menu_class' => 'NavList'
+                )); ?>
+            </div>
+
+            <div class="HeaderContainer">
+                <div class="HeaderContain Title">
+                    <?php if (!empty($maintitlehome)) : ?>
+                        <h1><?php echo ($maintitlehome); ?></h1>
+                    <?php endif; ?>
+                </div>
+                <div class="HeaderContain SubTitle">
+                    <?php if (!empty($secondtitlehome)) : ?>
+                        <h2><?php echo ($secondtitlehome); ?></h2>
+                    <?php endif; ?>
+                </div>
+                <div class="HeaderContain Button">
+                    <?php if (!empty($iconhome)) : ?>
+                        <a href="#prestahome"><img src="<?php echo ($iconhome['url']); ?>" alt="<?php echo ($iconhome['alt']); ?>" /></a>
+                    <?php endif; ?>
+                </div>
+            </div>
+        </header>
+
+    <?php else : ?> 
+
+        <header class="SecondHeader" style="background-image: url(<?php if (!empty($bgimghome)) : ?><?php echo ($bgimghome['url']); ?><?php endif; ?>) ">
+            <div class="HomeFilter"></div>
+            <div class="HeaderNav">
+                <div class="NavContainer Img">
+                    <?php if (!empty($logonav)) : ?>
+                        <a href="<?php echo (get_home_url()); ?>"><img src="<?php echo ($logonav['url']); ?>" alt="<?php echo ($logonav['alt']); ?>" /></a>
+                    <?php endif; ?>
+                </div>
+                <?php wp_nav_menu(array(
+                    'menu' => 'main_menu',
+                    'container_class' => 'NavContainer Nav',
+                    'menu_class' => 'NavList',
+                    'menu_id' => false
+                )); ?>
+            </div>
+
+            <div class="HeaderContainer">
+                <div class="HeaderContain Title">
+                    <?php if (!empty($maintitlehome)) : ?>
+                        <h1><?php echo ($maintitlehome); ?></h1>
+                    <?php endif; ?>
+                </div>
+            </div>
+        </header>
+
+    <?php endif; ?>
