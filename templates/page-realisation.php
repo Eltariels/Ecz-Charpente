63400f09942828eb6feedc50e125263f62589a74
Eltariels <theo.borella.hc@gmail.com>
2023-01-20 14:11:24 +0100

    update foreach réa

--- templates/page-realisation.php
+++ templates/page-realisation.php
@@ -9,41 +9,43 @@ get_header();
 
 $block_realisations = get_field('block_realisations');
 
-$titleblock_realisations = get_field('titleblock_realisations');
 
-$containblock_realisations = get_field('containblock_realisations');
 
-$linkblock_realisations = get_field('linkblock_realisations');
+//debug($block_realisations);
 
 ?>
 
 <main>
     <section>
         <?php foreach ($block_realisations as $block_realisation) : ?>
-            <article>
-                <?php foreach ($titleblock_realisations as $titleblock_realisation) : ?>
-                    <div>
-                        <h2><?php echo ($titleblock_realisation['titlecontainer_realisation']); ?></h2>
-                    </div>
-                <?php endforeach; ?>
+            <article class="RealisationPage MainBlock">
 
-                <div class="">
-                    <?php foreach ($containblock_realisations as $containblock_realisation) : ?>
-                        <div class="">
-                            <img src="<?php echo ($containblock_realisation['imgblock_realisation']['url']); ?>" alt="<?php echo ($containblock_realisation['imgblock_realisation']['alt']); ?>" width="<?php echo ($containblock_realisation['imgblock_realisation']['sizes']['imgblockrealisation-width']); ?>" height="<?php echo ($containblock_realisation['imgblock_realisation']['sizes']['imgblockrealisation-width']); ?>">
-                        </div>
-                        <div>
-                            <p><?php echo ($containblock_realisation['parablock_realisation']); ?></p>
+                <div class="RealisationPage Title">
+                    <h2 class=""><?php echo ($block_realisation['titleblock_realisations']); ?></h2>
+                </div>
+                
+                <div class="RealisationPage BlockContainer">
+                    <?php foreach ($block_realisation['containblock_realisations'] as $i => $containblock_realisation) : ?>
+                        <div class="RealisationPage BlockContain">
+                            <div class="ImgBlock">
+                                <div class="ImgOne">
+                                    <img src="<?php echo ($containblock_realisation['imgblockone_realisation']['url']); ?>" alt="<?php echo ($containblock_realisation['imgblockone_realisation']['alt']); ?>" width="<?php echo ($containblock_realisation['imgblockone_realisation']['sizes']['imgblockrealisation-width']); ?>" height="<?php echo ($containblock_realisation['imgblockone_realisation']['sizes']['imgblockrealisation-width']); ?>">
+                                </div>
+                                <div class="ImgTwo">
+                                    <img src="<?php echo ($containblock_realisation['imgblocktwo_realisation']['url']); ?>" alt="<?php echo ($containblock_realisation['imgblocktwo_realisation']['alt']); ?>" width="<?php echo ($containblock_realisation['imgblocktwo_realisation']['sizes']['imgblockrealisation-width']); ?>" height="<?php echo ($containblock_realisation['imgblocktwo_realisation']['sizes']['imgblockrealisation-width']); ?>">
+                                </div>
+                            </div>
+                            <div class="TextBlock">
+                                <p><?php echo ($containblock_realisation['parablock_realisation']); ?></p>
+                            </div>
                         </div>
                     <?php endforeach; ?>
                 </div>
 
-                <?php foreach ($linkblock_realisations as $linkblock_realisation) : ?>
-                    <div>
-                        <a href="<?php echo ($linkblock_realisation['linkcontainer_realisation']['url']); ?>">
-                            <?php echo ($linkblock_realisation['linkcontainer_realisation']['title']); ?></a>
-                    </div>
-                <?php endforeach; ?>
+                <div class="RealisationPage Link">
+                    <a href="<?php echo ($block_realisation['linkblock_realisations']['url']); ?>">
+                        <?php echo ($block_realisation['linkblock_realisations']['title']); ?></a>
+                </div>
             </article>
         <?php endforeach; ?>
     </section>
