38efc641ef5444f8a7b8266461d2f4650fd47440
Eltariels <theo.borella.hc@gmail.com>
2023-02-10 07:40:02 +0100

    Update page-solarpanel.php

--- templates/page-solarpanel.php
+++ templates/page-solarpanel.php
@@ -23,18 +23,20 @@ $imgcertification_solarpanels = get_field('imgcertification_solarpanel');
     <section>
         <?php foreach ($mainblocks_solarpanel as $mainblock_solarpanel) : ?>
             <article class="SolarpanelPage FirstSection">
-                <div class="title">
+                <div class="Title">
                     <h2 id="<?php echo (sanitize_title($mainblock_solarpanel['mainblock_solarpanel']['title_block_solarpanel'])); ?>"><?php echo ($mainblock_solarpanel['mainblock_solarpanel']['title_block_solarpanel']); ?></h2>
                 </div>
-                <div class="SolarpanelPage block">
-                    <div class="SolarpanelPage FirstBlockContain">
-                        <h3><?php echo ($mainblock_solarpanel['mainblock_solarpanel']['subtitle_block_solarpanel']); ?></h3>
-                        <div><?php echo ($mainblock_solarpanel['mainblock_solarpanel']['textarea_block_solarpanel']); ?></div>
+                <div class="SolarpanelPage BlockContainer">
+                    <div class="SolarpanelPage BlockContain">
+                        <div class="SolarpanelPage FirstBlockContain">
+                            <h3><?php echo ($mainblock_solarpanel['mainblock_solarpanel']['subtitle_block_solarpanel']); ?></h3>
+                            <div><?php echo ($mainblock_solarpanel['mainblock_solarpanel']['textarea_block_solarpanel']); ?></div>
+                        </div>
+                        <div class="SolarpanelPage SecondBlockContain">
+                            <img src="<?php echo ($mainblock_solarpanel['mainblock_solarpanel']['img_block_solarpanel']['url']); ?>" alt="<?php echo ($mainblock_solarpanel['mainblock_solarpanel']['img_block_solarpanel']['alt']); ?>" width="<?php echo ($mainblock_solarpanel['mainblock_solarpanel']['img_block_solarpanel']['sizes']['imgblockservice-width']); ?>" height="<?php echo ($mainblock_solarpanel['mainblock_solarpanel']['img_block_solarpanel']['sizes']['imgblockservice-height']); ?>">
+                        </div>
                     </div>
-                    <div class="SolarpanelPage SecondBlockContain">
-                        <img src="<?php echo ($mainblock_solarpanel['mainblock_solarpanel']['img_block_solarpanel']['url']); ?>" alt="<?php echo ($mainblock_solarpanel['mainblock_solarpanel']['img_block_solarpanel']['alt']); ?>" width="<?php echo ($mainblock_solarpanel['mainblock_solarpanel']['img_block_solarpanel']['sizes']['imgblockservice-width']); ?>" height="<?php echo ($mainblock_solarpanel['mainblock_solarpanel']['img_block_solarpanel']['sizes']['imgblockservice-height']); ?>">
-                    </div>
-                    <div class="SolarpanelPage ThridBlockContain">
+                    <div class="SolarpanelPage Link">
                         <a href="<?php echo ($mainblock_solarpanel['mainblock_solarpanel']['link_block_solarpanel']['url']); ?>">
                             <?php echo ($mainblock_solarpanel['mainblock_solarpanel']['link_block_solarpanel']['title']); ?></a>
                     </div>
@@ -43,11 +45,11 @@ $imgcertification_solarpanels = get_field('imgcertification_solarpanel');
         <?php endforeach; ?>
         <article class="SolarpanelPage SecondSection">
             <div class="SolarpanelPage Title">
-                <h2><?php echo ($titlecertification_solarpanel); ?></h2>
+                <h2 id="<?php echo (sanitize_title($titlecertification_solarpanel)); ?>"><?php echo ($titlecertification_solarpanel); ?></h2>
             </div>
             <div class="SolarpanelPage FirstBlockContain">
                 <p><?php echo ($sentencecertification_solarpanel); ?></p>
-                <div>
+                <div class="SolarpanelPage LogoCertif">
                     <?php foreach ($imgcertification_solarpanels as $imgcertification_solarpanel) : ?>
                         <div>
                             <img src="<?php echo ($imgcertification_solarpanel['img_certif_solarpanel']['url']); ?>" alt="<?php echo ($imgcertification_solarpanel['img_certif_solarpanel']['alt']); ?>" width="<?php echo ($imgcertification_solarpanel['img_certif_solarpanel']['sizes']['medium-width']); ?>" height="<?php echo ($imgcertification_solarpanel['img_certif_solarpanel']['sizes']['medium-height']); ?>">
