6dbcc34be8026b07490f9e3c2ac23cbafe21a46c
Eltariels <theo.borella.hc@gmail.com>
2023-02-10 07:37:44 +0100

    ajout footer php

--- footer.php
+++ footer.php
@@ -1,11 +1,51 @@
-        <footer>
+<?php
+
+$title_footernavlist = get_field('title_footernavlist', 'option');
+
+$subtitle_one_footernavlist = get_field('subtitle_one_footernavlist', 'option');
+
+$subtitle_two_footernavlist = get_field('subtitle_two_footernavlist', 'option');
+
+$subtitle_third_footernavlist = get_field('subtitle_third_footernavlist', 'option');
+
+$img_footer = get_field('img_footer', 'option');
+
+$first_text_footer = get_field('first_text_footer', 'option');
+
+$second_text_footer = get_field('second_text_footer', 'option');
+
+$third_text_footer = get_field('third_text_footer', 'option');
+
+// debug($img_footer);
+?>
+
+<footer>
+    <div class="FooterNav">
+        <div>
             <?php wp_nav_menu(array(
                 'menu' => ID_FOOTER_NAV,
+                'container_class' => 'FooterContainer',
+                'menu_class' => 'FooterListOne',
                 'menu_id' => false
             )); ?>
-            <p>&copy; All rights reserved to Mr.BORELLA Théo <?php echo (date('Y')); ?></p>
-        </footer>
-        <?php wp_footer(); ?>
-        </body>
+        </div>
+        <div class="ListFooterSupp">
+            <ul>
+                <li><a href="<?php echo ($title_footernavlist['url']); ?>"><?php echo ($title_footernavlist['title']); ?></a></li>
+                <li><?php echo ($subtitle_one_footernavlist) ?></li>
+                <li><?php echo ($subtitle_two_footernavlist) ?></li>
+                <li><?php echo ($subtitle_third_footernavlist) ?></li>
+            </ul>
+        </div>
+    </div>
+    <div class="FooterSubPart">
+        <img src="<?php echo ($img_footer['url']); ?>" alt="<?php echo ($img_footer['alt']); ?>" width="<?php echo ($img_footer['sizes']['imgblockservice-width']); ?>" height="<?php echo ($img_footer['sizes']['imgblockservice-height']); ?>">
+        <p class="FooterText"><?php echo ($first_text_footer) ?></p>
+        <a class="FooterText" href="<?php echo ($second_text_footer['url']); ?>"><?php echo ($second_text_footer['title']); ?></a>
+        <p class="FooterText"><?php echo ($third_text_footer) ?> <?php echo (date('Y')); ?></p>
+    </div>
+</footer>
+<?php wp_footer(); ?>
+</body>
 
-        </html>
+</html>
