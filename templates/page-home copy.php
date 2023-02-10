5d48dfb7934011399c831f0470913b5cf82d97d1
Eltariels <theo.borella.hc@gmail.com>
2023-02-10 07:39:28 +0100

    création d'une page "site en construction"

---
+++ templates/page-home copy.php
@@ -0,0 +1,30 @@
+<?php
+
+/* 
+    Template Name: Page home
+*/
+
+//------------------------------- HEADER ---------------------------
+get_header();
+
+
+
+?>
+
+<main>
+    <section>
+        <article class="HomePage FirstSection">
+            <div class="HomePage Title">
+                <h2 id="prestahome">!! --- SITE EN CONSTRUCTION --- !!</h2>
+            </div>
+        </article>
+    </section>
+</main>
+
+
+<?php
+
+//------------------------------- FOOTER ---------------------------
+get_footer();
+
+?>
